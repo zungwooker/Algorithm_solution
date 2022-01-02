e2ed5b5e515876bbf31b55102aa80c41a2ef99a5
zungwooker <zungwooker@gmail.com>
2022-01-02 17:53:31 +0900

    210102

--- boj/1092.py
+++ boj/1092.py
@@ -4,14 +4,33 @@ M = int(input())
 boxs = list(map(int, input().split()))
 
 if min(cranes) > max(boxs):
-    print(M//N+1)
+    print(-(-M//N))
     exit()
 elif max(cranes) < max(boxs):
     print(-1)
     exit()
 
-cranes.sort(reverse=True)
-boxs.sort(reverse=True)
+cranes.sort()
+boxs.sort()
 craneDuty = [0 for _ in range(N)]
 
+i = 0 # for crane
+j = 0 # for box
+leftBoxs = M
+movedBoxs = 0
 
+leftCranes = N
+doneCranes = 0
+ceiling = (leftBoxs - movedBoxs) / (leftCranes - doneCranes)
+
+while j < M:
+    if cranes[i] >= boxs[j] and craneDuty[i] < ceiling:
+        craneDuty[i] += 1
+        j += 1
+        movedBoxs += 1
+    else:
+        i += 1
+        doneCranes += 1
+        ceiling = (leftBoxs - movedBoxs) / (leftCranes - doneCranes)
+
+print(max(craneDuty))
