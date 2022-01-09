9d2fea0d27453c4b92a1cca42c5e3b3d2f4b60cb
Seo Jungwook <zungwooker@gmail.com>
2022-01-09 17:39:09 +0900

    220109

--- boj/16120.py
+++ boj/16120.py
@@ -1,35 +1,23 @@
-"""p = input()
-
-if len(p) < 4:
-    print("NP")
-    exit()
-
-word = p[:4]
-
-for x in p[4:]:
-    word += x
-    if word[-4:] == "PPAP":
-        word = word[:-3]
-
-if word == "P" or word == "PPAP":
-    print("PPAP")
-else:
-    print("NP")"""
-
 p = list(input())
+if p == ['P'] or p == p['P', 'P', 'A', 'P']:
+    print("PPAP")
+    exit()
+    
 if len(p) < 4:
     print("NP")
     exit()
 
-word = p[:4]
-
-for x in p[4:]: # 길이가 5이상
+word = p[:4] # 앞 4글자 입력
+print(word)
+for x in p[4:]: # 길이가 5이상, 5번째 글자부터 입력
     word.append(x)
+    print(word)
     if len(word) >= 4 and word[-4:] == ['P', 'P', 'A', 'P']:
         for _ in range(3):
             word.pop()
+        print(word)
     
-print(word)
+print("final",word)
 if word == ['P'] or word == ['P', 'P', 'A', 'P']:
     print("PPAP")
 else:
